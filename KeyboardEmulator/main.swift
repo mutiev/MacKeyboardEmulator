45833d83b1afa71f78c5d09a485017058d59f012
Федор Мутьев <mutiev@gmail.com>
2021-09-26 01:40:55 +0600

    Hello World me with the Swift

--- KeyboardEmulator/main.swift
+++ KeyboardEmulator/main.swift
@@ -7,5 +7,31 @@
 
 import Foundation
 
-print("Hello, World!")
+let source = CGEventSource(stateID: CGEventSourceStateID.hidSystemState)
 
+let cmdKey: UInt16 = 0x38
+let numberThreeKey: UInt16 = 0x14
+
+let cmdDown = CGEvent(keyboardEventSource: source, virtualKey: cmdKey, keyDown: true)
+let cmdUp = CGEvent(keyboardEventSource: source, virtualKey: cmdKey, keyDown: false)
+let keyThreeDown = CGEvent(keyboardEventSource: source, virtualKey: numberThreeKey, keyDown: true)
+let keyThreeUp = CGEvent(keyboardEventSource: source, virtualKey: numberThreeKey, keyDown: false)
+
+
+fileprivate func testShortcut() {
+
+    let loc = CGEventTapLocation.cghidEventTap
+    print("function executed")
+    cmdDown?.flags = CGEventFlags.maskCommand
+    cmdUp?.flags = CGEventFlags.maskCommand
+    keyThreeDown?.flags = CGEventFlags.maskCommand
+    keyThreeUp?.flags = CGEventFlags.maskCommand
+
+    cmdDown?.post(tap: loc)
+    keyThreeDown?.post(tap: loc)
+    cmdUp?.post(tap: loc)
+    keyThreeUp?.post(tap: loc)
+
+}
+print("sending hotkey")
+testShortcut()
